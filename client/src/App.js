30b9be07f1900307473ce63b57870d59a50e0add
Jimmy Tang <blay.tenshi@gmail.com>
2019-07-22 01:22:03 +1000

    Whoops forgot to add in /client

---
+++ client/src/App.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+class App extends React.Component {
+
+    state = {
+        message: ''
+    };
+
+    componentDidMount() {
+        const ws = new WebSocket("ws://localhost:8080");
+
+        ws.onopen = () => {
+            console.log('connection opened!');
+            ws.send(
+                JSON.stringify({
+                    event: 'events',
+                    data: 'something'
+                })
+            );
+        };
+
+        ws.onmessage = (data) => {
+            console.log(data.data);
+            this.setState({
+                message: data.data
+            });
+        };
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Welcome to Websockets you nerds!</h1>
+                <div>Message Received is: {this.state.message}</div>
+            </div>
+        )
+    }
+}
+
+export default App;
